c0478593b3faaad14c0d1460b01a1626b70ee92d
Darcy Buttrose <darcy.buttrose@gmail.com>
2014-04-21 21:05:15 +0800

    DB - split into individual routed pages

--- Web/App/main.js
+++ Web/App/main.js
@@ -1,6 +1,46 @@
 ﻿(function() {
     'use strict';
 
-    var main = angular.module('main', ["ngAnimate","ui"]);
+    var main = angular.module('main', ['ngAnimate', 'ui', 'ui.router']);
+
+    main.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: '/home',
+            templateUrl: 'header.html',
+            controller: 'HeaderCtrl'
+        }).state('about', {
+            url: '/about',
+            templateUrl: 'about.html',
+            controller: 'AboutCtrl'
+        }).state('calltoaction', {
+            url: '/calltoaction',
+            templateUrl: 'calltoaction.html'
+        }).state('contact', {
+            url: '/contact',
+            templateUrl: 'contact.html',
+            controller: 'ContactCtrl'
+        }).state('portfolio', {
+            url: '/portfolio',
+            templateUrl: 'portfolio.html',
+            controller: 'PortfolioCtrl'
+        }).state('services', {
+            url: '/services',
+            templateUrl: 'services.html'
+        }).state('sociallinks', {
+            url: '/sociallinks',
+            templateUrl: 'sociallinks.html'
+        }).state('testimonials', {
+            url: '/testimonials',
+            templateUrl: 'testimonials.html',
+            controller: 'TestimonialsCtrl'
+        }).state('twitter', {
+            url: '/twitter',
+            templateUrl: 'twitter.html',
+            controller: 'TwitterCtrl'
+        });
+        
+        $urlRouterProvider.otherwise('/home');
+    }]);
+
     return main;
 })()
