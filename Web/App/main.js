d2560a34d6aac597c668686c0ec4b6cafb4e024a
Darcy Buttrose <darcy.buttrose@gmail.com>
2014-04-24 21:00:32 +0800

    DB - fixed routes

--- Web/App/main.js
+++ Web/App/main.js
@@ -5,41 +5,30 @@
 
     main.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
         $stateProvider.state('home', {
-            url: '/home',
             templateUrl: 'header.html',
             controller: 'HeaderCtrl'
         }).state('about', {
-            url: '/about',
             templateUrl: 'about.html',
             controller: 'AboutCtrl'
         }).state('calltoaction', {
-            url: '/calltoaction',
             templateUrl: 'calltoaction.html'
         }).state('contact', {
-            url: '/contact',
             templateUrl: 'contact.html',
             controller: 'ContactCtrl'
         }).state('portfolio', {
-            url: '/portfolio',
             templateUrl: 'portfolio.html',
             controller: 'PortfolioCtrl'
         }).state('services', {
-            url: '/services',
             templateUrl: 'services.html'
         }).state('sociallinks', {
-            url: '/sociallinks',
             templateUrl: 'sociallinks.html'
         }).state('testimonials', {
-            url: '/testimonials',
             templateUrl: 'testimonials.html',
             controller: 'TestimonialsCtrl'
         }).state('twitter', {
-            url: '/twitter',
             templateUrl: 'twitter.html',
             controller: 'TwitterCtrl'
         });
-        
-        $urlRouterProvider.otherwise('/home');
     }]);
 
     return main;
